0a8ed6a04f8c21a9c96ddb94842aae93841fe207
Alex Christodoulou <amc8391@rit.edu>
2015-02-23 18:57:06 -0500

    Implemented a start for the FileHandler
    It now reads in files... it's pretty hype

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,4 +1,7 @@
 import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 /**
  * 
@@ -9,17 +12,32 @@ import java.io.File;
  *
  */
 public class FileHandler {
-
+		public FileHandler(File file){
+			readFile(file);
+		}
 		//needs a save (writeToFile) method and a load (readFile) method
 	
-		public String readFile(File file){
+		public Buffer readFile(File file){
+			Buffer buff = new Buffer();				// HTML Document Buffer
 			
-			return null;//place holder
+			try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+				String sCurrentLine = null;
+				Line l = new Line();					// Temporary new line
+				while ((sCurrentLine = br.readLine()) != null) { // Iterator that adds lines from file to buffer
+					l.setText(sCurrentLine);
+					buff.addLine(l);
+					System.out.println(l.toString());// TEST
+				}
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
 			
+			
+			return buff;
 		}
 		
 		public File writeToFile(String parsedBuffer){
-			
 			return null;//place holder
 		}
 		
