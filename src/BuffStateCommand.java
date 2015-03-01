c15e536750b515aebaa0e4cd383b62ee9a4f3917
Joe Diment <jvd5839@rit.edu>
2015-03-01 15:29:00 -0500

    Changed buffer, added a buffer state change command

---
+++ src/BuffStateCommand.java
@@ -0,0 +1,24 @@
+
+/*
+ * gets called when new text is entered into the buffer
+ */
+public class BuffStateCommand implements Command {
+	
+	private Buffer b;
+	private String s;
+	
+	public BuffStateCommand(Buffer buff, String newText) { 
+		b = buff;
+		s = newText;
+		
+	}
+	@Override
+	public void execute() {
+		b.addUndo(s);
+		b.addText(s);
+		
+		
+	}
+	
+
+}
