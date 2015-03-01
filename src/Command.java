4343133c3b7bf47fa803e5b4ca908f143780e43f
Alex Christodoulou <amc8391@rit.edu>
2015-03-01 14:15:31 -0500

    Added a few comments to Command

--- src/Command.java
+++ src/Command.java
@@ -10,6 +10,20 @@ public interface Command {
 	/**
 	 * Abstract method implying what needs to go into each command
 	 * Command pattern
+	 * 
+	 * Currently this should handle the following commands in the interface:
+	 * 		File		
+	 * 		New
+	 * 		Open
+	 * 		Save
+	 * 		Edit
+	 * 		Undo
+	 * 		Redo
+	 * 		Cut
+	 * 		Copy
+	 * 		Paste
+	 * 		Help
+	 * 		Open Readme
 	 */
     public abstract void execute();
 }
