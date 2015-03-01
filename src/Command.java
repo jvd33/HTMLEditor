550144c51d872ada1a6928fb658439efd7badae4
Alex Christodoulou <amc8391@rit.edu>
2015-03-01 14:11:55 -0500

    Added Command interface
    Starting on implementation of command pattern

---
+++ src/Command.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+/**
+ * @author Team Bash-Browns
+ *
+ */
+public interface Command {
+	/**
+	 * Abstract method implying what needs to go into each command
+	 * Command pattern
+	 */
+    public abstract void execute();
+}
