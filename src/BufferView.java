d4f319e8b71b1ed90682742808d88063f4ecf203
Dylan <dgr3547@rit.edu>
2015-03-01 16:15:08 -0500

    Added BufferView

---
+++ src/BufferView.java
@@ -0,0 +1,26 @@
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JEditorPane;
+
+
+public class BufferView  extends JEditorPane implements Observer{
+
+	private Buffer buffer; 
+	
+	public BufferView(Buffer b){
+		
+		this.buffer = b;
+		this.setText(buffer.toString());
+	}
+	
+	
+	
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+		
+}
