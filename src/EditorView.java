f4a7a80dce6039d6f999ad7363a3b5c43323ffa7
Alex Christodoulou <amc8391@rit.edu>
2015-03-04 06:27:34 -0500

    Big commit
    Moved stuff from EditorView to BufferView including:
    	Insert
        Save
        Undo
        Redo
        WordWrap
        Cut
        Copy
        Paste
        AutoIndent
    
    Got WordWrap working!
    Broke undo and redo :( but I'm working on it
    Got Save as... working in SaveCommand
    
    Known issues:
        Undo and Redo (Sorry!)
        AutoIndent
        Cut/Copy/Paste
        Insert
        Weird Swing stuff when loading new files

--- src/EditorView.java
+++ src/EditorView.java
@@ -1,10 +1,6 @@
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
-import java.awt.event.ContainerEvent;
-import java.awt.event.ContainerListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
@@ -36,31 +32,12 @@ public class EditorView extends JFrame implements Observer{
 		private	JMenuItem New;
 		private	JMenuItem open;
 		private	JMenuItem save;
-			
-		//Edit + items
-		private	JMenu edit;
-		private	JMenuItem undo;
-		private	JMenuItem redo;
-		private	JMenuItem cut;
-		private	JMenuItem copy ;
-		private	JMenuItem paste;
-		
-		//Insert + items
-		private JMenu insert;
-		private JMenuItem header;
-		private JMenuItem bold;
-		private JMenuItem italics;
-		private JMenuItem numberedList;
-		private JMenuItem bulletedList;
-		private JMenuItem dictList;
-		private JMenuItem table;
 
-			
 		// Help + items
 		private	JMenu help;
 		private	JMenuItem readme;
-			
-			
+
+
 	public EditorView(String title, HTMLEditor htmleditor){
 		super(title);
 		
@@ -82,25 +59,6 @@ public class EditorView extends JFrame implements Observer{
 		open = new JMenuItem("Open");
 		save = new JMenuItem("Save");
 		
-		//Edit + items
-		edit = new JMenu("Edit");
-		undo = new JMenuItem("Undo");
-		redo = new JMenuItem("Redo");
-		cut = new JMenuItem("Cut");
-		copy = new JMenuItem("Copy");
-		paste = new JMenuItem("Paste");
-		
-		//Insert + items
-		insert = new JMenu("Insert");
-		header = new JMenuItem("Header");
-		bold = new JMenuItem("Bold");
-		italics = new JMenuItem("Italics");
-		numberedList = new JMenuItem("Numbered List");
-		bulletedList = new JMenuItem("Bulleted List");
-		dictList = new JMenuItem("Dictionary List");
-		table = new JMenuItem("Table");
-
-		
 		// Help + items
 		help = new JMenu("Help");
 		readme = new JMenuItem("Open Readme");
@@ -117,47 +75,6 @@ public class EditorView extends JFrame implements Observer{
 		open.addActionListener(fileopen);
 		file.add(save);
 		save.addActionListener(filesave);
-		
-		menuBar.add(edit);
-		edit.add(undo);
-		undo.addActionListener(editundo);
-		edit.add(redo);
-		redo.addActionListener(editredo);
-		edit.addSeparator();
-		edit.add(cut);
-		cut.addActionListener(editcut);
-		edit.add(copy);
-		copy.addActionListener(editcopy);
-		edit.add(paste);
-		paste.addActionListener(editpaste);
-		
-		//Insert + items
-		menuBar.add(insert);
-		insert.add(header);
-		//header.addActionListener(insertHeader);
-		insert.add(bold);
-		//bold.addActionListener(insertBold);
-		insert.add(italics);
-		//italics.addActionListener(insertItalics);
-		insert.add(numberedList);
-		//numberedList.addActionListener(insertNumberedList);
-		insert.add(bulletedList);
-		//bulletedList.addActionListener(insertBulletedList);
-		insert.add(dictList);
-		//dictList.addActionListener(insertDictList);
-		insert.add(table);
-		//table.addActionListener(insertTable);
-		
-		header.addActionListener(insertButton);
-		bold.addActionListener(insertButton);
-		italics.addActionListener(insertButton);
-		numberedList.addActionListener(insertButton);
-		bulletedList.addActionListener(insertButton);
-		dictList.addActionListener(insertButton);
-		table.addActionListener(insertButton);
-
-
-		
 		
 		menuBar.add(help);
 		help.add(readme);
@@ -230,75 +147,15 @@ public class EditorView extends JFrame implements Observer{
 					String path = jfc.getSelectedFile().toString();
 					editor.getCurrentBuffer().setFile(path);
 					editor.notifyObservers();
-					Command save = new SaveCommand(editor, currentBuffer);
+					Command save = new SaveCommand(currentBuffer);
 					save.execute();
 				} catch(NullPointerException n) { 
 					System.out.println("No file entered");
 				}
 				
 			}
-			Command save = new SaveCommand(editor, currentBuffer);
+			Command save = new SaveCommand(currentBuffer);
 			save.execute();
-		}
-	};
-	
-	/*
-	 * Undo listener
-	 */
-	ActionListener editundo = new ActionListener(){
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			System.out.print("Undo Button pushed!");
-			Command undo = new UndoCommand(editor.getCurrentBuffer());
-			undo.execute();
-		}
-	};
-	
-	/*
-	 * redo listener
-	 */
-	ActionListener editredo = new ActionListener(){
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			System.out.print("Redo Button pushed!");
-			Command redo = new RedoCommand(editor.getCurrentBuffer());
-			redo.execute();
-		}
-	};
-	
-	/*
-	 * Cut listener
-	 */
-	ActionListener editcut = new ActionListener(){
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			System.out.print("Cut Button pushed!");
-			Command cut = new CutCommand("");
-			cut.execute();
-		}
-	};
-	
-	/*
-	 * Copy listener
-	 */
-	ActionListener editcopy = new ActionListener(){
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			System.out.print("Copy Button pushed!");
-			//Command copy = new CopyCommand();
-			//copy.execute();
-		}
-	};
-	
-	/*
-	 * Paste listener
-	 */
-	ActionListener editpaste = new ActionListener(){
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			System.out.print("Paste Button pushed!");
-			//Command paste = new PasteCommand();
-			//paste.execute();
 		}
 	};
 	
@@ -313,19 +170,7 @@ public class EditorView extends JFrame implements Observer{
 			//readmecommand.execute();
 		}
 	};
-	
-	/*
-	 * Insert listener
-	 */
-	ActionListener insertButton = new ActionListener(){
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			System.out.println("One of the insert buttons has been pressed");
-			//Command insertcommand = new InsertCommand("TAG DATA HERE");
-			//insertcommand.execute();
-		}
-	};
-	
+
 	/*
 	 * Buffer change listener
 	 */
@@ -334,7 +179,7 @@ public class EditorView extends JFrame implements Observer{
 		@Override
 		public void keyTyped(KeyEvent e) {
 			Buffer currentBuffer = editor.getCurrentBuffer();
-			String textInBuffView = ((BufferView) e.getSource()).getText();
+			String textInBuffView = ((BufferView) e.getSource()).getTextArea().getText();
 			//currentBuffer.addUndo(textInBuffView);
 			Command buffState = new BuffStateCommand(currentBuffer, textInBuffView);
 			buffState.execute();
