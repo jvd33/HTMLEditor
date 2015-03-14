1e173f2c606082e9b8a36a5002b97a2174aeacf0
nate6s <nwe8978@g.rit.edu>
2015-03-14 16:47:28 -0400

    CommandHandler class and Undoable interface complete, we just need to make our commands implement the Undoable interface

---
+++ src/CommandHandler.java
@@ -0,0 +1,50 @@
+
+public class CommandHandler {
+	
+	java.util.Stack<Undoable> undoables;
+	java.util.Stack<Undoable> redoables;
+	
+	public CommandHandler(){
+		this.undoables = new java.util.Stack<Undoable>();
+		this.redoables = new java.util.Stack<Undoable>();
+	}
+
+	public void executeCommand(Command c){
+		if(c instanceof Undoable){
+			undoables.push((Undoable)c);
+		}
+		c.execute();
+	}
+	
+	public void undoCommand(){
+		if(this.undoables.isEmpty()){
+			return;
+		}
+		Undoable c = (Undoable)this.undoables.pop();
+		c.undo();
+		redoables.push(c);
+	}
+	
+	public void redoCommand(){
+		if(this.redoables.isEmpty()){
+			return;
+		}
+		Undoable c = (Undoable)this.redoables.pop();
+		c.redo();
+		undoables.push(c);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+interface Undoable{
+
+	void undo();
+
+	void redo();
+	
+}
