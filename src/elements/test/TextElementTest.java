f0e1bfcaf4edea0b0d05d432d4afb10409908c83
Alex Michael Christodoulou Jr. <amc8391@rit.edu>
2015-03-17 21:43:17 -0400

    Tests of TextElement pushed

---
+++ src/elements/test/TextElementTest.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package elements.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import elements.TextElement;
+
+/**
+ * @author Team Bash-Browns
+ *
+ */
+public class TextElementTest {
+
+	/**
+	 * Test method for {@link elements.TextElement#print()}.
+	 */
+	@Test
+	public void testPrint() {
+		TextElement te = new TextElement("asdf");
+		assertEquals(te.print(), "asdf");
+	}
+
+	/**
+	 * Test method for {@link elements.TextElement#getChildren()}.
+	 */
+	@Test
+	public void testGetChildren() {
+		TextElement te = new TextElement("asdf");
+		assertNull(te.getChildren());
+	}
+
+	/**
+	 * Test method for {@link elements.TextElement#getEndTag()}.
+	 */
+	@Test
+	public void testGetEndTag() {
+		TextElement te = new TextElement("asdf");
+		assertEquals(te.getEndTag(), "");
+	}
+
+	/**
+	 * Test method for {@link elements.TextElement#equals(java.lang.Object)}.
+	 */
+	@Test
+	public void testEqualsObject() {
+		assertEquals(new TextElement("asdf"), new TextElement("asdf"));
+		assertNotEquals(new TextElement("asdf"), new TextElement("fdsa"));
+	}
+
+}
