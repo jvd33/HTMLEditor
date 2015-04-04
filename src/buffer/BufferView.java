1812ad6353369f67a20902257721c84aa159de05
Dylan <dgr3547@rit.edu>
2015-04-04 17:22:08 -0400

    So...found a fun new method that makes numbering the lines trivial...

--- src/buffer/BufferView.java
+++ src/buffer/BufferView.java
@@ -97,6 +97,9 @@ public class BufferView extends JPanel implements Observer{
 		undo = new JButton("Undo");
 		redo = new JButton("Redo");
 		wordwrap = new JButton("Toggle Word-Wrap");
+		wordwrap.setBackground(Color.GREEN);
+		wordwrap.setOpaque(true);
+		wordwrap.setBorderPainted(false);
 		newline = new JButton("Toggle AutoIndent");
 		inserttag = new JButton("Insert Tag...");
 		multipleindent = new JButton("Indent lines...");
@@ -182,15 +185,17 @@ public class BufferView extends JPanel implements Observer{
 	 * Word-Wrap toggle listener
 	 */
 	ActionListener wordWrapListener = new ActionListener(){
-		private Color GREEN;
-
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			commandHandler.executeCommand(new WordWrapCommand(textArea));
 			if (textArea.getLineWrap() == true){
-				wordwrap.setBackground(GREEN);
+				wordwrap.setBackground(Color.GREEN);
+				wordwrap.setOpaque(true);
+				wordwrap.setBorderPainted(false);
 			}else{
 				wordwrap.setBackground(null);
+				wordwrap.setOpaque(false);
+				wordwrap.setBorderPainted(true);
 			}
 		}
 	};
@@ -269,9 +274,9 @@ public class BufferView extends JPanel implements Observer{
 		JButton CollapseButton = new JButton();
 		CollapseButton.addActionListener(collapse);
 		collapsepanel.removeAll();
-		int bsize = buffer.getNumLines();
+		//int bsize = buffer.getNumLines();
 		String linenum;
-		for(int x = 0; x<=bsize; x++){
+		for(int x = 0; x<=textArea.getLineCount(); x++){
 			linenum = ""+(x+1);
 			collapsepanel.add(new JLabel(linenum));
 			collapsepanel.add(new CollapseButton(x+1));
